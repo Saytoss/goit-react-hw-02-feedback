a9a7ebd5de7fe56a94e7bf93c7441666cf5efd2b
Saytos <korana6277@gmail.com>
2020-07-28 23:32:42 +0300

    fix

---
+++ build/precache-manifest.d798696b09b32a45c5d0bfaf8f4510d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b35b1bfb54794253de17b1f8ba6c18d4",
+    "url": "/Saytoss/index.html"
+  },
+  {
+    "revision": "5c46cc0466eeacccba53",
+    "url": "/Saytoss/static/css/main.afcbec73.chunk.css"
+  },
+  {
+    "revision": "3adcd26bd6502f82171e",
+    "url": "/Saytoss/static/js/2.2e984201.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Saytoss/static/js/2.2e984201.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c46cc0466eeacccba53",
+    "url": "/Saytoss/static/js/main.1c80f3b5.chunk.js"
+  },
+  {
+    "revision": "c13d28f174bb6c861b17",
+    "url": "/Saytoss/static/js/runtime-main.9a9a6b94.js"
+  }
+]);
